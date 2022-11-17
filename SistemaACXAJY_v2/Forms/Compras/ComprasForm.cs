083398db1d87dd0582ade09cf0ea4e14607d9f07
Ángel Tejeda <angel.tejedat@gmail.com>
2022-11-17 17:47:48 -0600

    Ajustes al módulo de compras

--- SistemaACXAJY_v2/Forms/Compras/ComprasForm.cs
+++ SistemaACXAJY_v2/Forms/Compras/ComprasForm.cs
@@ -1,15 +1,14 @@
 ﻿using SistemaACXAJY_v2.Entities;
 using System.Data.SqlClient;
-using system_ACXAJY.Entities;
 using system_ACXAJY.Queries;
 
 namespace system_ACXAJY
 {
     public partial class ComprasForm : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=CMX-TST-3XA7HYU\SQLEXPRESS;Initial Catalog=System_ACXAJY;Integrated Security=True");
-        SqlCommand cm = new SqlCommand();
-        SqlDataReader dr;
+        private readonly SqlConnection con = new(@"Data Source=CMX-TST-3XA7HYU\SQLEXPRESS;Initial Catalog=System_ACXAJY;Integrated Security=True");
+        private readonly List<Compra> _compras = new();
+
         public ComprasForm()
         {
             InitializeComponent();
@@ -19,21 +18,44 @@ namespace system_ACXAJY
         public void LoadCompra()
         {
             dgvcompras.Rows.Clear();
-            cm = new SqlCommand("SELECT * FROM compra", con);
-            con.Open();
-            dr = cm.ExecuteReader();
-            while (dr.Read())
-            {
 
-                dgvcompras.Rows.Add(dr[0].ToString(), dr[1].ToString(), dr[2].ToString());
+            SqlCommand cm = new("SELECT * FROM compra", con);
+            SqlDataReader? dr = null;
+
+            try {
+                con.Open();
+                dr = cm.ExecuteReader();
+                while (dr.Read())
+                {
+                    Compra compra = new()
+                    {
+                        Idcompra = Convert.ToInt32(dr[0]),
+                        totalcompra = float.Parse(dr[1].ToString()!),
+                        FechaCompra = Convert.ToDateTime(dr[2]),
+                    };
+
+                    _compras.Add(compra);
+
+                    dgvcompras.Rows.Add(
+                        compra.Idcompra,
+                        compra.totalcompra,
+                        compra.FechaCompra);
+                }
             }
-            dr.Close();
-            con.Close();
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+                dr?.Close();
+            }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            ModuloCompras moduloCompras = new ModuloCompras();
+            ModuloCompras moduloCompras = new();
             moduloCompras.btnSave.Enabled = true;
             moduloCompras.btnUpdate.Enabled = false;
             moduloCompras.ShowDialog();
@@ -43,62 +65,56 @@ namespace system_ACXAJY
         private void dgvcompras_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             string colName = dgvcompras.Columns[e.ColumnIndex].Name;
+            Compra compra = _compras[e.RowIndex];
+
             if (colName == "editar")
             {
-                ModuloCompras moduloCompras = new ModuloCompras();
-                Compra compra = new Compra();
-                compra.Idcompra = Convert.ToInt32(dgvcompras.Rows[e.RowIndex].Cells[0].Value.ToString());
-                compra.totalcompra = float.Parse(dgvcompras.Rows[e.RowIndex].Cells[1].Value.ToString());
-                compra.FechaCompra = DateTime.Parse(dgvcompras.Rows[e.RowIndex].Cells[2].Value.ToString());
-
-
-                moduloCompras.compra = compra;
-                moduloCompras.LoadCompra();
+                ModuloCompras moduloCompras = new(compra);
                 moduloCompras.btnSave.Enabled = false;
                 moduloCompras.btnUpdate.Enabled = true;
                 moduloCompras.ShowDialog();
             }
             else if (colName == "eliminar")
             {
-                if (MessageBox.Show("¿Eliminar registro?", "Registro eliminado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                if (MessageBox.Show("¿Eliminar registro?", "Registro eliminado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
                 {
-                    // 1. Abrir conexión
-                    con.Open();
-                    // 2. Iniciar transacción
-                    SqlTransaction transaction = con.BeginTransaction();
-                    // 3. Consultar pedido_productos del pedido seleccionado
-                    int idcompra = Convert.ToInt32(dgvcompras.Rows[e.RowIndex].Cells[0].Value.ToString());
-                    List<CompraMaterial> compraMaterials = CompraMaterialQueries.ConsultarPorCompra(con, idcompra, transaction);
-                    // 4. Eliminar registros de pedido_productos con PedidoProductoQueries.EliminarProductos()
-                    bool EliminarCompra = CompraMaterialQueries.EliminarMaterial(compraMaterials, con, transaction);
-                    if (EliminarCompra == false)
-                    {
-                        transaction.Rollback();
-                        con.Close();
-                        return;
-                    }
-                    // 5. Eliminar pedido
-                    cm = new SqlCommand("DELETE FROM compra WHERE ID_compra = '" + idcompra + "'", con, transaction);
-                    try
-                    {
-                        cm.ExecuteNonQuery();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                        transaction.Rollback();
-                        con.Close();
-                        return;
-                    }
-
-                    // 6. Transaction.Commit
-                    transaction.Commit();
-                    // 7. Cerrar conexión
-                    con.Close();
-
-                    MessageBox.Show("Registro eliminado correctamente");
+                    return;
                 }
+
+                con.Open();
+                SqlTransaction transaction = con.BeginTransaction();
+
+                // Consultar productos de la venta
+                List<CompraMaterial> compraMateriales = CompraMaterialQueries.ConsultarPorCompra(con, compra.Idcompra, transaction);
+
+                // Eliminar los materiales
+                bool materialesEliminados = CompraMaterialQueries.EliminarMaterial(compraMateriales, con, transaction);
+                if (!materialesEliminados)
+                {
+                    transaction.Rollback();
+                    con.Close();
+                    return;
+                }
+
+                // Eliminar compra
+                SqlCommand cm = new($"DELETE FROM compra WHERE ID_compra = '{compra.Idcompra}'", con, transaction);
+                try
+                {
+                    cm.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    transaction.Rollback();
+                    con.Close();
+                    return;
+                }
+
+                transaction.Commit();
+                con.Close();
+                MessageBox.Show("Registro eliminado correctamente");
             }
+
             LoadCompra();
         }
     }
--- SistemaACXAJY_v2/Forms/Compras/ModuloCompras.cs
+++ SistemaACXAJY_v2/Forms/Compras/ModuloCompras.cs
@@ -8,43 +8,58 @@ namespace system_ACXAJY
 {
     public partial class ModuloCompras : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=CMX-TST-3XA7HYU\SQLEXPRESS;Initial Catalog=System_ACXAJY;Integrated Security=True");
-        SqlCommand cm = new SqlCommand();
-        SqlDataReader dr;
-        List<Material> ListaMaterial = new List<Material>();
-        List<CompraMaterial> ListaCompraMaterialActual = new List<CompraMaterial>();
-        List<CompraMaterial> ListaCompraMaterialOriginal = new List<CompraMaterial>();
-        public Compra compra = null;
-        public ModuloCompras()
+        private readonly SqlConnection con = new(@"Data Source=CMX-TST-3XA7HYU\SQLEXPRESS;Initial Catalog=System_ACXAJY;Integrated Security=True");
+
+        private readonly List<Material> _listaMaterial = new();
+        private readonly List<CompraMaterial> _listaCompraMaterialActual = new();
+        private readonly List<CompraMaterial> _listaCompraMaterialOriginal = new();
+        private readonly Compra _compra;
+
+        public ModuloCompras(Compra? compra = null)
         {
             InitializeComponent();
             LoadMaterial();
 
+            if (compra == null)
+            {
+                _compra = new Compra();
+            }
+            else
+            {
+                _compra = compra;
+                LoadCompra();
+            }
         }
+
         public void clear()
         {
             dTimeCompra.Value = DateTime.Now;
         }
+
         public void LoadMaterial()
         {
             coBoxMat.Items.Clear();
             coBoxMat.DisplayMember = "NombreMaterial";
 
-            cm = new SqlCommand("SELECT ID_material, nombre_mat FROM material", con);
+            SqlDataReader? dr = null;
+            SqlCommand cm = new("SELECT ID_material, nombre_mat FROM material", con);
             try
             {
                 con.Open();
                 dr = cm.ExecuteReader();
+
                 while (dr.Read())
                 {
-                    Material material = new Material();
-                    material.Idmaterial = Convert.ToInt32(dr[0].ToString());
-                    material.NombreMaterial = dr[1].ToString();
+                    Material material = new()
+                    {
+                        Idmaterial = Convert.ToInt32(dr[0].ToString()),
+                        NombreMaterial = dr[1].ToString()!
+                    };
+
                     coBoxMat.Items.Add(material);
-                    ListaMaterial.Add(material);
+                    _listaMaterial.Add(material);
                 }
             }
-
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
@@ -52,10 +67,10 @@ namespace system_ACXAJY
             finally
             {
                 con.Close();
-                dr.Close();
+                dr?.Close();
             }
         }
-        
+
         private void pictureBoxExit_Click(object sender, EventArgs e)
         {
             this.Dispose();
@@ -68,15 +83,14 @@ namespace system_ACXAJY
             List<CompraMaterial> compraMaterials = CompraMaterialQueries.ConsultarPorCompra(con, idcompra);
             foreach (CompraMaterial compraMaterial in compraMaterials)
             {
-                Material material = ListaMaterial
-                    .Where(p => p.Idmaterial == compraMaterial.idmaterialcm)
-                    .FirstOrDefault()!;
+                Material material = _listaMaterial
+                    .Find(p => p.Idmaterial == compraMaterial.idmaterialcm)!;
 
                 float preciomaterial = (float) material.PrecioMaterial;
                 string nombrematerial = material.NombreMaterial;
 
-                ListaCompraMaterialActual.Add(compraMaterial);
-                ListaCompraMaterialOriginal.Add(compraMaterial);
+                _listaCompraMaterialActual.Add(compraMaterial);
+                _listaCompraMaterialOriginal.Add(compraMaterial);
 
                 dgvSeleccionMat.Rows.Add(0, nombrematerial, compraMaterial.cantmaterialcm, preciomaterial);
             }
@@ -86,20 +100,25 @@ namespace system_ACXAJY
         }
         public void LoadCompra()
         {
-            if (compra == null)
+            if (_compra == null)
             {
                 return;
             }
 
             // 1. Cargar información del pedido en el formulario
-            lblCID.Text = compra.Idcompra.ToString();
-            txtTotalPagar.Text = compra.totalcompra.ToString();
-            dTimeCompra.Text = compra.FechaCompra.ToString();
-
-
+            lblCID.Text = _compra.Idcompra.ToString();
+            txtTotalPagar.Text = _compra.totalcompra.ToString();
+            dTimeCompra.Text = _compra.FechaCompra.ToString();
 
             // 2. Consultar los productos del pedido
-            LoadCompraMaterial(compra.Idcompra);
+            LoadCompraMaterial(_compra.Idcompra);
+
+            // Calcular el total de compra
+            double total = _listaCompraMaterialActual.Sum(cm => {
+                Material material = _listaMaterial.Find(m => m.Idmaterial == cm.idmaterialcm)!;
+                return cm.cantmaterialcm * material.PrecioMaterial;
+            });
+            txtTotalPagar.Text = total.ToString();
         }
 
         private void btnClear_Click(object sender, EventArgs e)
@@ -111,102 +130,87 @@ namespace system_ACXAJY
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            CompraMaterial compraMaterial = new CompraMaterial();
-            Material SelectedMat = coBoxMat.SelectedItem as Material;
-            compraMaterial.idmaterialcm = SelectedMat.Idmaterial;
-            compraMaterial.cantmaterialcm = Convert.ToInt32(txtCantMat.Text);
+            if (coBoxMat.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione un material");
+                return;
+            }
 
-            ListaCompraMaterialActual.Add(compraMaterial);
-            dgvSeleccionMat.Rows.Add(0, SelectedMat.NombreMaterial, compraMaterial.cantmaterialcm, SelectedMat.PrecioMaterial);
+            Material materialSeleccionado = (Material)coBoxMat.SelectedItem!;
 
-            
+            CompraMaterial compraMaterial = new()
+            {
+                idmaterialcm = materialSeleccionado.Idmaterial,
+                cantmaterialcm = Convert.ToInt32(txtCantMat.Text)
+            };
+
+            _listaCompraMaterialActual.Add(compraMaterial);
+            dgvSeleccionMat.Rows.Add(0, materialSeleccionado.NombreMaterial, compraMaterial.cantmaterialcm, materialSeleccionado.PrecioMaterial);
 
             coBoxMat.SelectedIndex = -1;
             txtprecioMat.Clear();
             txtCantMat.Clear();
-        }
-        public void MaterialSeleccionado()
-        {
-            dgvSeleccionMat.Rows.Clear();
-            cm = new SqlCommand(@"
-                SELECT ID_detallecm, nombre_mat, cantmaterial_cm, precio_mat
-                FROM compra_material
-                INNER JOIN material ON ID_material_cm = ID_material", con);
 
-            try
-            {
-                con.Open();
-                dr = cm.ExecuteReader();
-                while (dr.Read())
-                {
-
-                    dgvSeleccionMat.Rows.Add(dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString());
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                dr.Close();
-                con.Close();
-            }
+            // Sumar el precio de los productos a la venta
+			double total = Convert.ToDouble(txtTotalPagar.Text);
+			total += compraMaterial.cantmaterialcm * materialSeleccionado.PrecioMaterial;
+			txtTotalPagar.Text = total.ToString();
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("¿Guardar registro?", "Registro Guardado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            if (MessageBox.Show("¿Guardar registro?", "Registro Guardado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
             {
-                con.Open();
-                SqlTransaction transaction = con.BeginTransaction();
+                return;
+            }
 
-                // 1. Guardar la compra
-                cm = new SqlCommand(@"
+            con.Open();
+            SqlTransaction transaction = con.BeginTransaction();
+
+            // 1. Guardar la compra
+            string query = @"
                     INSERT INTO compra(total_compra, fecha_compra)
                     VALUES(@total_compra, @fecha_compra);
-                    SELECT SCOPE_IDENTITY();", con, transaction);
-                cm.Parameters.AddWithValue("@total_compra", Convert.ToInt32(txtTotalPagar.Text));
-                cm.Parameters.AddWithValue("@fecha_compra", dTimeCompra.Value);
+                    SELECT SCOPE_IDENTITY();";
 
-                // 2. Obtener el ID del pedido
-                int idcompra = 0;
-                try
-                {
-                    object returnObj = cm.ExecuteScalar();
-                    if (returnObj != null)
-                    {
-                        int.TryParse(returnObj.ToString(), out idcompra);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    transaction.Rollback();
-                    con.Close();
-                    MessageBox.Show(ex.Message);
-                    return;
-                }
+            SqlCommand cm = new(query, con, transaction);
+            cm.Parameters.AddWithValue("@total_compra", Convert.ToInt32(txtTotalPagar.Text));
+            cm.Parameters.AddWithValue("@fecha_compra", dTimeCompra.Value);
 
-                // 3. Agregar los productos a la tabla pedido_producto
-                bool agregados = CompraMaterialQueries.AgregarMaterial(
-                    idcompra,
-                    ListaCompraMaterialActual,
-                    con,
-                    transaction);
-
-                if (agregados == false)
-                {
-                    transaction.Rollback();
-                    con.Close();
-                    return;
-                }
-
-                transaction.Commit();
-                con.Close();
-
-                MessageBox.Show("Registro Guardado Correctamente");
-                this.Dispose();
+            // 2. Obtener el ID del pedido
+            int idCompra = 0;
+            try
+            {
+                object returnObj = cm.ExecuteScalar();
+                idCompra = int.Parse(returnObj.ToString()!);
             }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                con.Close();
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            // 3. Agregar los productos a la tabla pedido_producto
+            bool agregados = CompraMaterialQueries.AgregarMaterial(
+                idCompra,
+                _listaCompraMaterialActual,
+                con,
+                transaction);
+
+            if (!agregados)
+            {
+                transaction.Rollback();
+                con.Close();
+                return;
+            }
+
+            transaction.Commit();
+            con.Close();
+
+            MessageBox.Show("Registro Guardado Correctamente");
+            Dispose();
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
@@ -215,10 +219,12 @@ namespace system_ACXAJY
             SqlTransaction transaction = con.BeginTransaction();
 
             // 1. Actualizar el pedido
-            cm = new SqlCommand(@"
+            string query = @$"
                 UPDATE compra
                 SET fecha_compra=@fecha_compra, total_compra=@total_compra
-                WHERE Id_compra =" + compra.Idcompra, con, transaction);
+                WHERE Id_compra = {_compra.Idcompra}";
+
+            SqlCommand cm = new(query, con, transaction);
 
             cm.Parameters.AddWithValue("@fecha_compra", dTimeCompra.Value);
             cm.Parameters.AddWithValue("@total_compra", txtTotalPagar.Text);
@@ -237,11 +243,11 @@ namespace system_ACXAJY
 
             // 2. Eliminar los productos que ya estaban
             bool eliminados = CompraMaterialQueries.EliminarMaterial(
-                ListaCompraMaterialOriginal,
+                _listaCompraMaterialOriginal,
                 con,
                 transaction);
 
-            if (eliminados == false)
+            if (!eliminados)
             {
                 transaction.Rollback();
                 con.Close();
@@ -250,12 +256,12 @@ namespace system_ACXAJY
 
             // 3. Agregar los nuevos productos
             bool agregados = CompraMaterialQueries.AgregarMaterial(
-                compra.Idcompra,
-                ListaCompraMaterialActual,
+                _compra.Idcompra,
+                _listaCompraMaterialActual,
                 con,
                 transaction);
 
-            if (agregados == false)
+            if (!agregados)
             {
                 transaction.Rollback();
                 con.Close();
@@ -266,13 +272,21 @@ namespace system_ACXAJY
             con.Close();
 
             MessageBox.Show("Compra Actualizado");
-            this.Dispose();
+            Dispose();
         }
 
         private void dgvSeleccionMat_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            ListaCompraMaterialActual.RemoveAt(e.RowIndex);
+            CompraMaterial compraMaterial = _listaCompraMaterialActual[e.RowIndex];
+            Material material = _listaMaterial.Find(m => m.Idmaterial == compraMaterial.idmaterialcm)!;
+
+            _listaCompraMaterialActual.RemoveAt(e.RowIndex);
             dgvSeleccionMat.Rows.RemoveAt(e.RowIndex);
+
+            // Restar el precio de los materiales a la compra
+			double total = Convert.ToDouble(txtTotalPagar.Text);
+			total -= compraMaterial.cantmaterialcm * material.PrecioMaterial;
+			txtTotalPagar.Text = total.ToString();
         }
     }
 }
